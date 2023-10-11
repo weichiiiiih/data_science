2c27c088780b7b427681bb77b02d55005fcbc96c
weichiiiiih <aaii04300218@gmail.com>
2023-10-11 14:14:57 +0800

    Update 1011.R
    第一堂下課嚕

--- 1011.R
+++ 1011.R
@@ -7,3 +7,38 @@ myMusicGrande <- c(10,9)
 
 flights <- jsonlite::fromJSON("data/international_flights.json")
 View(flights)
+
+
+##data information -----
+data1 <- list(
+  file = "data/international_flights.json", ##紀錄資料路徑
+  meta = list( ##記錄資料訊息
+    name = "國際航空定期時刻表",
+    source_link = "https://data.gov.tw/dataset/161167") ##紀錄資料來源
+)
+
+flights <- list(
+  data = list(data1)
+)
+
+data1$file
+data1$meta$name
+data1$meta$source_link
+
+##data practice
+
+
+data2$file_place
+data2$information$name
+data2$information$source_link
+
+data2 <- list(
+  file_place = "data/international_flights.json"
+  information = list(
+    name = "國際航空定期時刻表",
+    source_link ="https://data.gov.tw/dataset/161167") 
+  
+)
+
+
+saveRDS(something, ..... )
