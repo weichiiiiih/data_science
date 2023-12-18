65d9ad16aeac794c5143bf3fdca943d8fde3d3fd
weichiiiiih <aaii04300218@gmail.com>
2023-12-18 15:27:58 +0800

    12345

--- 組作2.R
+++ 組作2.R
@@ -12,18 +12,14 @@ library(tibble)
 install.packages("ggplot2")
 library(ggplot2)
 
+
+
 #104年進站----
 numberV1 <- data$V1 |> as.numeric()
 sortnumberV1 <- sort(numberV1, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV1)
 #1(總計), 44  43  74  80  77  63  71  78  70  49 
 data[c(3,46,45,76,82,79,65,73,80,72,51), c(1,2)] #前10
-
-#結果的表格
-in104top <- data[c(28, 14, 61, 12, 60, 23, 103, 26, 15, 11), c(1, 4)]
-in104top_table <- as.data.frame(in104top)
-print(in104top_table)
-
 
 sortnumber1V <- sort(numberV1, decreasing=F,index.return = TRUE)
 print(sortnumber1V)
@@ -104,29 +100,50 @@ numberV7 <- data$V7 |> as.numeric()
 sortnumberV7 <- sort(numberV7, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV7)
 #   1  44  74  43  80  77  70  71  63  78  38
-data[c(3,46,76,45,82,79,72,73,65,80,40), c(1,8)]
+data[c(3,46,76,45,82,79,72,73,65,80,40), c(1,8)] #前10
 
+sortnumber7V <- sort(numberV7, decreasing=F,index.return = TRUE)
+print(sortnumber7V)
+#26  12  59  10  21  58  13   9 101  24 
+data[c(28,14,61,12,23,60,15,11,103,26), c(1,8)] #後十
 
 #107年出站----
 numberV8 <- data$V8 |> as.numeric()
 sortnumberV8 <- sort(numberV8, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV8)
 #   1  44  74  43  80  70  77  71  63  78  38 
-data[c(3,46,76,45,82,72,79,73,65,80,40), c(1,9)]
+data[c(3,46,76,45,82,72,79,73,65,80,40), c(1,9)] #前十
+
+sortnumber8V <- sort(numberV8, decreasing=F,index.return = TRUE)
+print(sortnumber8V)
+#12  26  59  10  21  58   9  93  13 101 
+data[c(14,28,61,12,23,60,11,95,15,103), c(1,9)] #後十
+
 
 #108年進站----
 numberV9 <- data$V9 |> as.numeric()
 sortnumberV9 <- sort(numberV9, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV9)
 # 1  44  74  43  80  77  70  71  63  38  87 
-data[c(3,46,76,45,82,79,72,73,65,40,89), c(1,10)]
+data[c(3,46,76,45,82,79,72,73,65,40,89), c(1,10)] #前十
+
+sortnumber9V <- sort(numberV9, decreasing=F,index.return = TRUE)
+print(sortnumber9V)
+#12  59  10  26  21  58  13 101   9  24
+data[c(14,61,12,28,23,60,15,103,11,26), c(1,10)] #後十
+
 
 #108年出站----
 numberV10 <- data$V10 |> as.numeric()
 sortnumberV10 <- sort(numberV10, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV10)
 #  1  44  74  80  43  70  77  71  63  38  45 
-data[c(3,46,76,82,45,72,79,73,65,40,47), c(1,11)]
+data[c(3,46,76,82,45,72,79,73,65,40,47), c(1,11)] #前十
+
+sortnumber10V <- sort(numberV10, decreasing=F,index.return = TRUE)
+print(sortnumber10V)
+#12  10  59  26  21  58   9 101  13  93
+data[c(14,12,61,28,23,60,11,103,15,95), c(1,11)] #後十
 
 
 #109年進站----
@@ -134,7 +151,14 @@ numberV11 <- data$V11 |> as.numeric()
 sortnumberV11 <- sort(numberV11, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV11)
 #   1  44  74  80  43  70  71  77  38  63  45 
-data[c(3,46,76,82,45,72,73,79,40,65,47), c(1,12)]
+data[c(3,46,76,82,45,72,73,79,40,65,47), c(1,12)] #前十
+
+
+sortnumber11V <- sort(numberV11, decreasing=F,index.return = TRUE)
+print(sortnumber11V)
+#12  59  10  21  58  26 100  13   9   2 
+data[c(14,61,12,23,60,28,102,15,11,4), c(1,12)] #後十
+
 
 
 #109年出站----
@@ -142,7 +166,13 @@ numberV12 <- data$V12 |> as.numeric()
 sortnumberV12 <- sort(numberV12, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV12)
 # 1  44  80  74  43  70  71  77  38  45  78 
-data[c(3,46,82,76,45,72,73,79,40,47,80), c(1,13)]
+data[c(3,46,82,76,45,72,73,79,40,47,80), c(1,13)] #前十
+
+sortnumber12V <- sort(numberV12, decreasing=F,index.return = TRUE)
+print(sortnumber12V)
+#12  10  59  21  58  26   9  93 100  13  
+data[c(14,12,61,23,60,28,11,95,102,15), c(1,13)] #後十
+
 
 
 #110年進站----
@@ -150,7 +180,12 @@ numberV13 <- data$V13 |> as.numeric()
 sortnumberV13 <- sort(numberV13, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV13)
 # 1  44  80  74  43  71  77  70  63  38  45 
-data[c(3,46,82,76,45,73,79,72,65,40,47), c(1,14)]
+data[c(3,46,82,76,45,73,79,72,65,40,47), c(1,14)] #前十
+
+sortnumber13V <- sort(numberV13, decreasing=F,index.return = TRUE)
+print(sortnumber13V)
+# 12  59  10  26 100  21   2  58  13   9 
+data[c(14,61,12,28,102,23,4,60,15,11), c(1,14)] #後十
 
 
 #110年出站----
@@ -158,7 +193,13 @@ numberV14 <- data$V14 |> as.numeric()
 sortnumberV14 <- sort(numberV14, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV14)
 #1(總計), 44  80  74  43  70  71  77  45  38  78
-data[c(3,46,82,76,45,72,73,79,47,40,80), c(1,15)]
+data[c(3,46,82,76,45,72,73,79,47,40,80), c(1,15)] #前十
+
+sortnumber14V <- sort(numberV14, decreasing=F,index.return = TRUE)
+print(sortnumber14V)
+#12  10  59  21  26   2  58  93   9  13 
+data[c(14,12,61,23,28,4,60,95,11,15), c(1,15)] #後十
+
 
 
 #111年進站----
@@ -166,7 +207,13 @@ numberV15 <- data$V15 |> as.numeric()
 sortnumberV15 <- sort(numberV15, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV15)
 #1(總計), 44  80  74  43  71  70  77  45  63  38 
-data[c(3,46,82,76,45,73,72,79,47,65,40), c(1,16)]
+data[c(3,46,82,76,45,73,72,79,47,65,40), c(1,16)] #前十
+
+sortnumber15V <- sort(numberV15, decreasing=F,index.return = TRUE)
+print(sortnumber15V)
+#12  59  10  21 100  26  58   2  13   9 
+data[c(14,61,12,23,102,28,60,5,15,11), c(1,16)] #後十
+
 
 
 #111年出站----
@@ -174,8 +221,12 @@ numberV16 <- data$V16 |> as.numeric()
 sortnumberV16 <- sort(numberV16, decreasing=TRUE,index.return = TRUE)
 print(sortnumberV16)
 #1(總計), 44  80  74  43  70  45  71  77  38  78
-data[c(3,46,80,76,45,72,47,73,79,40,80), c(1,17)]
+data[c(3,46,80,76,45,72,47,73,79,40,80), c(1,17)] #前十
 
+sortnumber16V <- sort(numberV16, decreasing=F,index.return = TRUE)
+print(sortnumber16V)
+#  12  59  10  21  26  58   2  93   9  13 
+data[c(14,61,12,23,28,60,4,95,11,15), c(1,17)] #後十
 
 
 #安裝並載入折線圖的package:gcookbook----
@@ -277,6 +328,7 @@ print(paste("相關係數",cor108))
 
 #前10進站的折線圖
 top10_data <- data[c(3,46,45,76,82,79,65,73,80,72,51,89,47,40), ]
+View(top10_data)
 
 #添加列名稱
 colnames(top10_data) <- c(
@@ -295,43 +347,19 @@ colnames(top10_data) <- c(
   "頂溪" )
 
 
-# 使用 ggplot2 创建折线图
-ggplot(top10_data, aes(x = colnames(top10_data), y = value)) +
-  geom_line(aes(color = variable), size = 1) +
-  labs(title = "折线图", x = "行", y = "值") +
-  theme_minimal()
+#把重合的站點分開來看----
+excel_file <-("/Users/weichiiiiih/Desktop/data_science/data/metro.xlsx")
+copyline38 <- readxl::read_excel(excel_file, sheet = 1)
+# 复制第38列的数据并添加为新的一列
+data[["中正紀念堂R線"]] <- data[[38]]
 
-
-
-
-
-
-
+# 显示结果
+print(data)
 
 #分組-------
 #安裝
-#install.packages(c("readxl", "dplyr"))
-#library(readxl)
-#library(dplyr)
-
-
+install.packages(c("readxl", "dplyr"))
+library(readxl)
+library(dplyr)
 install.packages("dplyr")
-
-V10data <- transform(data,
-                  Group = case_when(
-                    row_number() %in%c(66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87) ~ "板南線",
-                    row_number() %in% c(44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,88,89,90,91,92,113) ~ "淡水信義線",
-                    row_number %in% c(100,101,102,103,104,105,106,107,108,109,110,111,112,114,115,116,117,118,119,120) ~ "中和新蘆線",
-                    row_number %in% c(28,29,30,31,32,33,34,35,36,37,38,39,93,94,95,96,97,98,99) ~ "松山新店線",
-                    row_number %in% c(4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30) ~ "文湖線"
-                  ))
-                 
-# 按 Group 列分组，计算每组的统计量
-summary_statistics <- aggregate(V10 ~ Group, V10data, function(x) c(Count = length(x), Mean = mean(x), Median = median(x), Max = max(x), Min = min(x)))
-
-
-
-# 显示结果
-print(summary_statistics)
-
 
